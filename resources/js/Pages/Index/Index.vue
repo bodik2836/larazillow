a9ec39f772bcd25ab9b7de2fc0a523c2880185b0
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-17 17:33:59 +0300

    Passed data to pages

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
+defineProps({
+    message: String
+})
 </script>
 
 <template>
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
+    <p>Message: {{ message }}</p>
 </template>
 
 <style scoped>
