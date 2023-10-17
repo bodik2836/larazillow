4272a16808a4ee9912aa1b712668fd0055d23027
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-17 16:59:29 +0300

    Created first pages

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div>Index</div>
+    <Link href="/hello">Show Page</Link>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Index/Show.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <div>Show</div>
+    <Link href="/">Index Page</Link>
+</template>
+
+<style scoped>
+
+</style>
