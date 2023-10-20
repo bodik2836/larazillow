99f383378abefe9e81be3187df576d19272e813d
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-20 12:00:46 +0300

    Separated reusable elements to components

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,8 +1,19 @@
 <script setup>
+import { Link } from '@inertiajs/vue3'
+import ListingAddress from "@/Components/ListingAddress.vue";
 
+defineProps({
+    listings: Array,
+})
 </script>
 
 <template>
+
+<div  v-for="listing in listings" :key="listing.id">
+    <Link :href="`/listings/${listing.id}`">
+        <ListingAddress :listing="listing" />
+    </Link>
+</div>
 
 </template>
 
--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,9 +1,15 @@
 <script setup>
+import ListingAddress from "@/Components/ListingAddress.vue";
 
+defineProps({
+    listing: Object,
+})
 </script>
 
 <template>
-
+<div>
+    <ListingAddress :listing="listing" />
+</div>
 </template>
 
 <style scoped>
