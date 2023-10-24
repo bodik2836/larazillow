f6579b5f86569f2f1d7a5fc80f650eb1480ae7c5
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-24 16:59:14 +0300

    Added new ui components (Box, ListingSpace)

---
+++ resources/js/Components/UI/Box.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="border border-gray-200 dark:border-gray-800 rounded-md p-4 shadow-sm dark:text-gray-300">
+    <slot />
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { Link } from '@inertiajs/vue3'
 import ListingAddress from "@/Components/ListingAddress.vue"
+import Box from "@/Components/UI/Box.vue"
+import ListingSpace from "@/Components/UI/ListingSpace.vue"
 
 defineProps({
     listings: Array,
@@ -8,21 +10,22 @@ defineProps({
 </script>
 
 <template>
-
-<div  v-for="listing in listings" :key="listing.id">
-    <div>
-        <Link :href="route('listings.show', {listing: listing.id})">
-            <ListingAddress :listing="listing" />
-        </Link>
-    </div>
-    <div>
-        <Link :href="route('listings.edit', {listing: listing.id})">Edit</Link>
-    </div>
-    <div>
-        <Link :href="route('listings.destroy', {listing: listing.id})" method="DELETE" as="button">Delete</Link>
-    </div>
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="listing.id">
+        <div>
+            <Link :href="route('listings.show', {listing: listing.id})">
+                <ListingSpace :listing="listing" class="text-lg" />
+                <ListingAddress :listing="listing" class="text-gray-500" />
+            </Link>
+        </div>
+        <div>
+            <Link :href="route('listings.edit', {listing: listing.id})">Edit</Link>
+        </div>
+        <div>
+            <Link :href="route('listings.destroy', {listing: listing.id})" method="DELETE" as="button">Delete</Link>
+        </div>
+    </Box>
 </div>
-
 </template>
 
 <style scoped>
