0a3f505dced741d6958ecaa018f5305574f193cf
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-10-24 13:25:37 +0300

    Styled header and flash messages

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -8,21 +8,33 @@ const flashSuccess = computed(() => page.props.flash.success)
 </script>
 
 <template>
-<div>
-    <Link href="/listings">Listings</Link>&nbsp;
-    <Link href="/listings/create">New Listing</Link>
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
+        <div class="container mx-auto">
+            <nav class="p-4 flex item-center justify-between">
+                <div class="text-lg font-medium">
+                    <Link :href="route('listings.index')">Listings</Link>
+                </div>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href="route('listings.index')">LaraZillow</Link>
+                </div>
+                <div>
+                    <Link
+                        :href="route('listings.create')"
+                        class="bg-indigo-600 hover:bg-indigo-500 text-white font-medium p-2 rounded-md"
+                    >+ New Listing</Link>
+                </div>
+            </nav>
+        </div>
+    </header>
 
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-    </div>
+    <main class="container mx-auto p-4">
+        <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+            {{ flashSuccess }}
+        </div>
 
-    <slot></slot>
-</div>
+        <slot></slot>
+    </main>
+
 </template>
 
-<style scoped>
-.success {
-    background-color: green;
-    color: white;
-}
-</style>
+<style scoped></style>
