26ba25dd69afd809d3c213163f0c1af9c16a5b8f
Bohdan Tarasiuk <bodik2836@gmail.com>
2023-11-01 11:56:46 +0200

    Added user_id column to listings table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Listing;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Listing::factory(20)->create();
+        User::factory(1)->create();
+        Listing::factory(20)->create(['user_id' => 1]);
     }
 }
