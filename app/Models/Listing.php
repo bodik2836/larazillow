b72307d3d35c6150c122a0ed7b8d96c2d24a60ad
Bohdan Tarasiuk <bodik2836@gmail.com>
2024-02-02 14:45:27 +0200

    Changed count of listings per page

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use http\QueryString;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
